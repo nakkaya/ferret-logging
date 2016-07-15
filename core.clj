f8b42215974e5b1c8f38228f254caa8d08b736c8
Nurullah Akkaya <nurullah@nakkaya.com>
2016-07-15 20:10:40 +0300

    implement init-from-text

--- core.clj
+++ core.clj
@@ -4,6 +4,15 @@
       "INITIALIZE_EASYLOGGINGPP"
       ""))
 
+(defnative init-from-text [txt]
+  (on "defined FERRET_STD_LIB"
+      ("ferret-logging/easylogging++.h")
+      "INITIALIZE_EASYLOGGINGPP"
+      "el::Configurations c;
+       c.setToDefault();
+       c.parseFromText(txt.cast<String>()->to_string());
+       el::Loggers::reconfigureAllLoggers(c);"))
+
 (defnative info [msg]
   (on "defined FERRET_STD_LIB"
       "LOG(INFO) << msg.cast<String>()->to_string();"))
